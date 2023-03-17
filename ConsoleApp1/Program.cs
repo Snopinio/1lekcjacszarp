2e47ea7c0b5fab4f8211c274e42684fc2e41c4b1
Snopinio <jansnopkowski64@gmail.com>
2023-03-17 12:19:55 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿//poporos uzytkownika o podanie imienia
+Console.WriteLine("Podaj swoje imię: ");
+//deklarujemy nowa zmienna typu tekstowego o nazwie "name"
+string name = "";
+//odczytamy od uzytkownika i zapiszemy do zmiennej imie - zapisz nieznajomy jesli uzytkowink nie udzieli odpowiedzi
+name = Console.ReadLine() ?? "Nieznajomy";
+//nowa zmienna - powitanie
+string greeting = "Witaj " + name +"!";
+//wpisz gotowe powitanie
+Console.WriteLine(greeting);
